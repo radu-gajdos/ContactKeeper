269feb0315a38af8c159ba0786c2a7b1e916aeb0
Radu <radugajdos123@gmail.com>
2024-03-09 13:13:59 +0200

    connected to a MongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require("./config/db")
 
 const app = express();
+
+//DB Connection
+connectDB();
 
 app.get('/', (req, res) => res.json({msg: "ContactKeeper API"}))
 
